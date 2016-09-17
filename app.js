222eef293f6ceca16936099a2cb1286c4cb96925
Darren Yin <Darren Yin>
2016-09-16 17:43:14 -0700

    hey pug works, I have no idea why it didn't work before

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var pug = require('pug');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,7 +27,7 @@ socket.init()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
